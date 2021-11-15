be65f46bd468e3cb0664e50f84cf6d9c31fc7d4f
Olamidun <kolapoolamidun@gmail.com>
2021-11-15 18:44:11 +0100

    gave url paths name paramater

--- items/urls.py
+++ items/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 app_name = 'items'
 
 urlpatterns = [
-    path('create_item', views.CreateItemAPIView.as_view()),
-    path('all_items', views.ListAllItemsAPIView.as_view()),
-    path('list_items_for_sellers', views.LisAllItemsForSellerAPIView.as_view()),
+    path('create_item', views.CreateItemAPIView.as_view(), name='create_item'),
+    path('all_items', views.ListAllItemsAPIView.as_view(), name='list_items'),
+    path('list_items_for_sellers', views.LisAllItemsForSellerAPIView.as_view(), name='list_items_for_user'),
     path('<int:id>', views.ItemDetailAPIView.as_view()),
-    path('list_all_items_for_buyer', views.ItemsForABuyer.as_view()),
+    path('list_all_items_for_buyer', views.ItemsForABuyer.as_view(), name='list_all_items_for_buyers'),
 ]

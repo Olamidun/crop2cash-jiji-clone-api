27669ec60b5abd193a514b46ea4a3b731b1a2f2b
Olamidun <kolapoolamidun@gmail.com>
2021-11-16 16:35:27 +0100

    removed unwanted code

--- buyers/views.py
+++ buyers/views.py
@@ -7,10 +7,6 @@ from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from .serializers import CreateInterestedBuyerSerializer
-# Create your views here.
-
-# class CreateBuyerAPIView(generics.CreateAPIView):
-#     serializer_class = CreateInterestedBuyerSerializer
 
 class CreateBuyerForItemAPIView(APIView):
     # This decorator ensures the request body is displayed on swagger

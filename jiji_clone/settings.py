c73fdbce606ce1dc2e3b528f333307a7a7ecbcf1
Olamidun <kolapoolamidun@gmail.com>
2021-11-16 15:54:09 +0100

    updated settings

--- jiji_clone/settings.py
+++ jiji_clone/settings.py
@@ -72,7 +72,7 @@ AUTH_USER_MODEL = 'seller.Seller'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware"
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
